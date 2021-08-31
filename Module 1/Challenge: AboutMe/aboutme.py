7ad389381cc034935cac07e587501d729dd5f0b9
Brett <67874400+MackleBJ@users.noreply.github.com>
2021-08-30 19:43:50 -0500

    Create aboutme.py

---
+++ Module 1/Challenge: AboutMe/aboutme.py
@@ -0,0 +1,17 @@
+# Brett M.
+# Aug 26, 2021
+# INFOTC 4401 - 01
+
+
+my_name = "Brett M."
+my_age = 29
+location_raised = "Missouri"
+current_occupation = "Veterans United - Intern / Student Veteran"
+previous_occupation = "US Navy Submarine Nuclear Analytical Chemist"
+fun_fact = "I've spent over 2 years under the surface of the ocean."
+life_goals = "I want to get a degree in Computer Science and start a career in the software developement field."
+
+print(my_name)
+print("{} years old\nFrom: {}\n".format(my_age, location_raised))
+print("Current Occupation: {}\nPrevious Occupation: {}\n".format(current_occupation, previous_occupation))
+print("Goals: {}\nFun Fact: {}".format(life_goals,fun_fact))
